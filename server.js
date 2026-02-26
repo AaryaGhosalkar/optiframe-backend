c1a4f8ec72c5fe5db49a55870162eb8c42896485
Aarya Ghosalkar <aaryaghosalkar19@gmail.com>
2026-02-26 23:34:56 +0530

    final

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/payment", paymentRoutes);
 app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 /* ================= START SERVER ================= */
